4a8ac2075a0d586772ac1f690a8b88a836196535
Jason <yfmlutz@gmail.com>
2025-09-18 22:30:24 -0400

    Export JwtModule to make JwtService available globally

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,6 +12,6 @@ import { AuthController } from './auth.controller';
   ],
   controllers: [AuthController],
   providers: [AuthService],
-  exports: [AuthService],
+  exports: [AuthService, JwtModule], // Add JwtModule to exports
 })
 export class AuthModule {}
